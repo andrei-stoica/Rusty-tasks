2caa88ad437173dd3f291cc824a39672c9875474
Andrei Stoica <andrei.stoica.365@gmail.com>
2023-06-15 08:57:19 -0400

    seperated module into new file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
+mod todo_file;
+
+use crate::todo_file::TodoFile;
 use chrono::naive::NaiveDate;
 use chrono::{Datelike, Local};
 use std::env;
 use std::fs::{copy, read_dir};
 use std::path::{Path, PathBuf};
 use std::process::Command;
-use todo_file::TodoFile;
 
 //TODO handle unwraps and errors more uniformly
-//TODO move TodoFile into its file
 //TODO clean up verbose printing
 
 fn main() {
@@ -49,80 +50,6 @@ fn main() {
                 .expect(format!("failed to launch editor {}", "vim").as_str());
         }
         _ => println!("Could not get today's date"),
-    }
-}
-
-mod todo_file {
-    use chrono::naive::NaiveDate;
-    use regex::Regex;
-    use std::convert::TryFrom;
-    use std::fs::DirEntry;
-    use std::str::FromStr;
-
-    #[derive(Debug)]
-    pub struct TodoFile {
-        pub file: DirEntry,
-        pub date: NaiveDate,
-    }
-
-    impl TodoFile {
-        fn capture_as_number<T: FromStr>(
-            capture: &regex::Captures,
-            name: &str,
-        ) -> Result<T, String> {
-            Ok(capture
-                .name(name)
-                .unwrap()
-                .as_str()
-                .parse::<T>()
-                .ok()
-                .ok_or("Something went wrong".to_owned())?)
-        }
-
-        pub fn latest_file(a: TodoFile, b: TodoFile) -> TodoFile {
-            if a.date > b.date {
-                a
-            } else {
-                b
-            }
-        }
-
-        fn get_file_regex() -> Regex {
-            //TODO This would ideally be configurable
-            Regex::new(r"(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2}).md")
-                .expect("could not create regex")
-        }
-    }
-
-    impl TryFrom<DirEntry> for TodoFile {
-        type Error = String;
-
-        fn try_from(direntry: DirEntry) -> Result<Self, Self::Error> {
-            let re = TodoFile::get_file_regex();
-            println!("{:?}", re);
-            let file_name = direntry.file_name();
-            let file_name_str = match file_name.to_str() {
-                Some(name) => name,
-                _ => "",
-            };
-            println!("{:?}", file_name_str);
-
-            if let Some(caps) = re.captures(file_name_str) {
-                let year: i32 = Self::capture_as_number(&caps, "year").unwrap();
-                let month: u32 = Self::capture_as_number(&caps, "month").unwrap();
-                let day: u32 = Self::capture_as_number(&caps, "day").unwrap();
-
-                return Ok(Self {
-                    file: direntry,
-                    date: NaiveDate::from_ymd_opt(year, month, day).unwrap(),
-                });
-            };
-            Err(format!(
-                "Could not parse file name => {{ name: {:?}, re: {:?} }}",
-                file_name, re
-            )
-            .to_string())
-        }
     }
 }
 
